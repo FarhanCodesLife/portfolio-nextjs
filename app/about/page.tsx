b7b411eb7df4cb8a7fdc3128ece2285b5c3a7889
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-10-15 14:54:53 +0500

    ok done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,11 +7,9 @@ const Page = () => {
     <React.Fragment>
       <div className=''>
 
-{/* Hero Section */}
 <h1 className='text-6xl m-10 font-bold text-center text-white   p-10'>About</h1>
 <div className="h-[80vh] bg-gradient-to-r gap-6 from-purple-800 to-purple-950 flex flex-col md:flex-row items-center justify-center px-4">
 
-{/* Image Section */}
 <div className="w-96 h-96 md:w-80 md:h-80  my-10 border-4 flex items-center justify-center border-[#ae8eb3] rounded-2xl overflow-hidden shadow-xl transition-transform transform hover:scale-105">
   <Image
     src={heroSectionImage}
@@ -23,7 +21,6 @@ const Page = () => {
 </div>
 
 
-{/* Text Section */}
 <div className="flex flex-col items-center md:items-start text-center md:text-left mb- md:mb-0 md:mr-8 space-y-6 animate-fade-in-up">
   <h1 className="text-4xl md:text-6xl font-bold text-white ">
     <span className="text-[#ba69c5]">Know How</span> I'M
@@ -35,12 +32,10 @@ const Page = () => {
 </div>
 
 
-{/* Skills Section */}
 <div className="bg-white py-10 px-4 md:px-10 mt-10">
 <h2 className="text-3xl md:text-6xl font-bold text-purple-800 text-center mb-10 ">My Skills</h2>
 
 <div className="grid mt-10 grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 justify-center items-center text-center">
-  {/* Skill - HTML */}
   <div className="flex flex-col items-center">
     <div className="w-20 h-20 rounded-full bg-[#f06529] flex items-center justify-center">
       <span className="text-white text-2xl font-bold">HTML</span>
@@ -48,7 +43,6 @@ const Page = () => {
     <p className="text-lg text-gray-700 mt-2">HTML5</p>
   </div>
 
-  {/* Skill - CSS */}
   <div className="flex flex-col items-center">
     <div className="w-20 h-20 rounded-full bg-[#2965f1] flex items-center justify-center">
       <span className="text-white text-2xl font-bold">CSS</span>
@@ -56,7 +50,6 @@ const Page = () => {
     <p className="text-lg text-gray-700 mt-2">CSS3</p>
   </div>
 
-  {/* Skill - JavaScript */}
   <div className="flex flex-col items-center">
     <div className="w-20 h-20 rounded-full bg-[#f7df1e] flex items-center justify-center">
       <span className="text-black text-2xl font-bold">JS</span>
@@ -64,7 +57,6 @@ const Page = () => {
     <p className="text-lg text-gray-700 mt-2">JavaScript</p>
   </div>
 
-  {/* Skill - TypeScript */}
   <div className="flex flex-col items-center">
     <div className="w-20 h-20 rounded-full bg-[#3178c6] flex items-center justify-center">
       <span className="text-white text-2xl font-bold">TS</span>
