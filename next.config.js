8dc1bbf1c5973756dabe7726e3f7173777238b0d
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-11-24 16:25:48 +0500

    firebase deploy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['your-image-domain.com'], // Add your image domains here
-  },
-  // If you're using experimental features, make sure they're properly configured
-  experimental: {
-    appDir: true,
-  },
-}
-
-module.exports = nextConfig 
+module.exports = {
+  output: 'export',
+};
